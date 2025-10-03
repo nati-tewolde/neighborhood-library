53740df78a4882659ca7b2bc8326a8a7ac60c36e
nati-tewolde <natisnewemail@gmail.com>
2025-10-03 13:43:28 -0400

    created Book class

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Book {
+
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkOutTo;
+
+
+
+}
